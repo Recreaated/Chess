e9f88d3504cd746abcf2073b116ffc6199fef356
Recreaated <youalreaadyknow@gmail.com>
2025-06-04 13:28:06 -0700

    Update Piece.java
    Updated verision of piece now working

--- Piece.java
+++ Piece.java
@@ -3,7 +3,16 @@ public class Piece {
     public Piece(String name){
         this.name = name;
     }
+    public Piece(){
+        this.name = null;
+    }
+    public String getName(){
+        return name;
+    }
 
     public void movesAvaliable(){}
-    public int[] possibleChecks(){}
+    public int[] possibleChecks(){
+        int[] output = {-1,-1};
+        return output;
+    }
 }
